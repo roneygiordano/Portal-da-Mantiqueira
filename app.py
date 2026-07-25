0e5719363eff6941025b436eea45dc2ece156f94
roneygiordano <roneygiordano@gmail.com>
2026-07-25 13:01:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ if not st.session_state.logado:
         # 📸 Renderiza a imagem e o título 100% centralizados no computador e no celular
         st.html(f"""
             <div style="text-align: center; margin-bottom: 15px;">
-                <img src="data:image/png;base64,{imagem_base64}" style="width: 120px; height: auto;">
-                <h2 style='font-size: 22px; font-weight: bold; letter-spacing: 2px; color: #FFFFFF; margin: 15px 0 20px 0;'>PORTAL DIGITAL</h2>
+                <img src="data:image/png;base64,{imagem_base64}" style="width: 70px; height: auto;">
+                <h2 style='font-size: 30px; font-weight: bold; letter-spacing: 2px; color: #FFFFFF; margin: 15px 0 20px 0;'>PORTAL 219 DIGITAL</h2>
             </div>
         """)
     except FileNotFoundError:
